439c6b66b437cb7fdccd4609b589db52cdfc8188
jezzy jumble <jezz@jezzy.dev>
2025-10-16 02:31:20 -0700

    start with example dioxus code to make sure env is set up correctly

--- simple-palettes/src/main.rs
+++ simple-palettes/src/main.rs
@@ -1,5 +1,4 @@
 #![feature(more_qualified_paths)]
-#![feature(unqualified_local_imports)]
 #![feature(supertrait_item_shadowing)]
 #![feature(non_exhaustive_omitted_patterns_lint)]
 #![feature(must_not_suspend)]
@@ -10,4 +9,40 @@
 #![allow(clippy::missing_errors_doc, reason = "todo: documentation")]
 #![allow(clippy::redundant_pub_crate, reason = "a fuckton of false positives")]
 
-const fn main() {}
+use dioxus::prelude::*;
+
+const FAVICON: Asset = asset!("/assets/favicon.ico");
+const MAIN_CSS: Asset = asset!("/assets/main.css");
+const HEADER_SVG: Asset = asset!("/assets/header.svg");
+const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
+
+fn main() {
+    launch(App);
+}
+
+#[component]
+fn App() -> Element {
+    rsx! {
+        document::Link { rel: "icon", href: FAVICON }
+        document::Link { rel: "stylesheet", href: MAIN_CSS } document::Link { rel: "stylesheet", href: TAILWIND_CSS }
+        Hero {}
+    }
+}
+
+#[component]
+pub fn Hero() -> Element {
+    rsx! {
+        div {
+            id: "hero",
+            img { src: HEADER_SVG, id: "header" }
+            div { id: "links",
+                a { href: "https://dioxuslabs.com/learn/0.6/", "📚 Learn Dioxus" }
+                a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
+                a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
+                a { href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
+                a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
+                a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
+            }
+        }
+    }
+}
