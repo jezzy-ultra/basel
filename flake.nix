115db032592db7ed5c5274ecf7ee529cbf7cff95
jezzy jumble <jezz@jezzy.dev>
2025-10-30 20:04:46 -0700

    streamline flake and add support for building with nix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 {
   description = "full-fat color scheme engine. write once, theme everything.";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     rust-overlay = {
@@ -13,12 +12,13 @@
     {
       nixpkgs,
       rust-overlay,
-      ...
+      self,
     }:
     let
       inherit (nixpkgs) lib;
       eachSystem = lib.genAttrs lib.systems.flakeExposed;
-
+      cargo = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+      workspace = cargo.workspace.package;
       pkgsFor = eachSystem (
         system:
         import nixpkgs {
@@ -28,141 +28,84 @@
       );
     in
     {
+      packages = eachSystem (
+        system:
+        let
+          pkgs = pkgsFor.${system};
+          rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+          rustPlatform = pkgs.makeRustPlatform {
+            cargo = rustToolchain;
+            rustc = rustToolchain;
+          };
+        in
+        {
+          default = rustPlatform.buildRustPackage {
+            pname = "basel";
+            version = workspace.version;
+            src = lib.cleanSource ./.;
+            cargoLock.lockFile = ./Cargo.lock;
+
+            nativeBuildInputs = with pkgs; [
+              clang
+              mold
+              pkg-config
+            ];
+
+            buildInputs = with pkgs; [
+              openssl
+              zlib
+            ];
+
+            RUSTFLAGS = "-C link-arg=-fuse-ld=mold";
+
+            meta = with lib; {
+              description = workspace.description;
+              homepage = workspace.repository;
+              license = workspace.license;
+              mainProgram = "basel";
+              platforms = platforms.unix;
+            };
+          };
+        }
+      );
+
       devShells = eachSystem (
         system:
         let
           pkgs = pkgsFor.${system};
-
           rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-
-          linuxPackages = lib.optionals pkgs.stdenv.isLinux [
-            pkgs.atk
-            pkgs.cairo
-            pkgs.clang
-            pkgs.fontconfig
-            pkgs.freetype
-            pkgs.fribidi
-            pkgs.gdk-pixbuf
-            pkgs.glib
-            pkgs.gtk3
-            pkgs.harfbuzz
-            pkgs.libsoup_3
-            pkgs.mold
-            pkgs.openssl
-            pkgs.pango
-            pkgs.pixman
-            pkgs.wayland
-            pkgs.wayland-protocols
-            pkgs.webkitgtk_4_1
-            pkgs.xorg.libX11
-            pkgs.xorg.libXcursor
-            pkgs.xorg.libXdamage
-            pkgs.xorg.libXext
-            pkgs.xorg.libXfixes
-            pkgs.xorg.libXi
-            pkgs.xorg.libXrandr
-            pkgs.xorg.libXrender
-            pkgs.zlib
-          ];
-
-          darwinPackages = lib.optionals pkgs.stdenv.isDarwin (
-            with pkgs.darwin.apple_sdk.frameworks;
-            [
-              IOKit
-              Carbon
-              WebKit
-              Security
-              Cocoa
-              CoreFoundation
-            ]
-          );
+          pkg = self.packages.${system}.default;
         in
         {
           default = pkgs.mkShell {
+            inputsFrom = [ pkg ];
+
             nativeBuildInputs = [
               rustToolchain
-              pkgs.pkg-config
-              pkgs.openssl
-            ]
-            ++ linuxPackages
-            ++ darwinPackages;
+            ];
+
+            RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
+            RUST_BACKTRACE = "1";
 
             shellHook = ''
-              export RUST_SRC_PATH="${rustToolchain}/lib/rustlib/src/rust/library"
-
               ${lib.optionalString pkgs.stdenv.isLinux ''
-                export PKG_CONFIG_PATH="${
-                  lib.concatStringsSep ":" [
-                    "${pkgs.atk.dev}/lib/pkgconfig"
-                    "${pkgs.cairo.dev}/lib/pkgconfig"
-                    "${pkgs.fontconfig.dev}/lib/pkgconfig"
-                    "${pkgs.freetype.dev}/lib/pkgconfig"
-                    "${pkgs.fribidi.dev}/lib/pkgconfig"
-                    "${pkgs.gdk-pixbuf.dev}/lib/pkgconfig"
-                    "${pkgs.glib.dev}/lib/pkgconfig"
-                    "${pkgs.gtk3.dev}/lib/pkgconfig"
-                    "${pkgs.harfbuzz.dev}/lib/pkgconfig"
-                    "${pkgs.libsoup_3.dev}/lib/pkgconfig"
-                    "${pkgs.openssl.dev}/lib/pkgconfig"
-                    "${pkgs.pango.dev}/lib/pkgconfig"
-                    "${pkgs.wayland.dev}/lib/pkgconfig"
-                    "${pkgs.webkitgtk_4_1.dev}/lib/pkgconfig"
-                    "${pkgs.xorg.libX11.dev}/lib/pkgconfig"
-                    "${pkgs.xorg.libXcursor.dev}/lib/pkgconfig"
-                    "${pkgs.xorg.libXdamage.dev}/lib/pkgconfig"
-                    "${pkgs.xorg.libXext.dev}/lib/pkgconfig"
-                    "${pkgs.xorg.libXfixes.dev}/lib/pkgconfig"
-                    "${pkgs.xorg.libXi.dev}/lib/pkgconfig"
-                    "${pkgs.xorg.libXrandr.dev}/lib/pkgconfig"
-                    "${pkgs.xorg.libXrender.dev}/lib/pkgconfig"
-                  ]
-                }"
-
-                export LD_LIBRARY_PATH="${
-                  lib.makeLibraryPath [
-                    pkgs.atk
-                    pkgs.cairo
-                    pkgs.clang
-                    pkgs.fontconfig
-                    pkgs.freetype
-                    pkgs.fribidi
-                    pkgs.gdk-pixbuf
-                    pkgs.glib
-                    pkgs.gtk3
-                    pkgs.harfbuzz
-                    pkgs.libsoup_3
-                    pkgs.mold
-                    pkgs.openssl
-                    pkgs.pango
-                    pkgs.pixman
-                    pkgs.wayland
-                    pkgs.wayland-protocols
-                    pkgs.webkitgtk_4_1
-                    pkgs.xorg.libX11
-                    pkgs.xorg.libXcursor
-                    pkgs.xorg.libXdamage
-                    pkgs.xorg.libXext
-                    pkgs.xorg.libXfixes
-                    pkgs.xorg.libXi
-                    pkgs.xorg.libXrandr
-                    pkgs.xorg.libXrender
-                    pkgs.zlib
-                  ]
-                }"
+                export LD_LIBRARY_PATH="${lib.makeLibraryPath pkg.buildInputs}:''${LD_LIBRARY_PATH:-}"
               ''}
 
               ${lib.optionalString pkgs.stdenv.isDarwin ''
-                export PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig"
+                export DYLD_LIBRARY_PATH="${lib.makeLibraryPath pkg.buildInputs}:''${DYLD_LIBRARY_PATH:-}"
               ''}
 
-              echo ""
-              echo "basel developer environment active"
-              echo "rust: $(rustc --version)"
-              echo "  on: ${system}"
-              echo ""
+               echo ""
+               echo "basel developer environment active"
+               echo "rust: $(rustc --version)"
+               echo "  on: ${system}"
+               echo ""
             '';
           };
         }
       );
+
+      formatter = eachSystem (system: pkgsFor.${system}.nixfmt-rfc-style);
     };
 }
